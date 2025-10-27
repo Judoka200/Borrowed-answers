2c4e50cb35be863363ee5a62de6e6eb4bfc754a3
Judoka200 <isaac.cundy@outlook.com>
2025-10-27 16:31:19 +0000

    new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+
+int main(){
+cout << "\033[31;42;7mThis is red text\054\304 \033[0m";
+// cin.ignore(4);
+
+cout<<"hello";
+
+}
