bfae7a09ad87c9b29c0e58e85ab796bca794a818
Judoka200 <isaac.cundy@outlook.com>
2025-10-29 16:04:09 +0000

    deleted:    main.cpp 	deleted:    main.exe
        new:colours.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ using namespace std;
 
 
 int main(){
-cout << "\033[31;42;7mThis is red text\054\304 \033[0m";
-// cin.ignore(4);
-
+cout << "\033[91;42;5mThis is red text\054\304 \033[0m";
+cout << "\033[38;5;74mherl";
 cout<<"hello";
-
+cout << "\033[2m low intensity \033[0m";
+cout << "\033[1m test text\033[0m";
+cin.ignore(1);
 }
