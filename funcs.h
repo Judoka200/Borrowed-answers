f049a5faa9618e5da186af5ed4e5af23c85f7054
Judoka200 <isaac.cundy@outlook.com>
2025-11-04 02:22:25 +0000

    modified:   funcs.h 	modified:   main.cpp

--- funcs.h
+++ funcs.h
@@ -31,3 +31,27 @@ void outputARY(int array[ROWS][COLS])
 }*/
 
 
+template <int ROWS, int COLS>
+void fillARY(int array[ROWS][COLS],int reset = 0)
+//if reset is set to anything other than 0, Set all values in array to 0 
+//reset = 0 therfore argument is optional and will not reset the function by default 
+{
+    if(reset){
+        for(int row = 0; row < ROWS; row++){
+            for(int col = 0; col < COLS; col++){
+                array[row][col] = 0;
+            }
+        }
+    }else{
+     int value = 0;  // or start with 1 if you prefer
+    for(int row = 0; row < ROWS; row++){
+        for(int col = 0; col < COLS; col++){
+            array[row][col] = value;
+            value++;
+          }
+        }
+    }
+}
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,30 +1,36 @@
 #include "funcs.h"
-
-//initialise size of dungeon (set the width and height)
-const int dungeonRowSize = 5;
-const int dungeonColSize = 10;
-  int dungeonlayout[dungeonRowSize][dungeonColSize];
-
 using namespace std;
 
+//initialise size of dungeon (set the width and height)
+const int dungeonRowSize =5;
+const int dungeonColSize = 3;
+int dungeonlayout[dungeonRowSize][dungeonColSize]; //initialise the array for the board
+
+
+//Function Calls
 void dungeonSetup(int dungeonlayout[dungeonRowSize][dungeonColSize]);
 
+
+
+
+
 int main(){
-    int stepsRemaining = -1;  // setup  
+    int stepsRemaining = -1;  // setup 
 
 dungeonSetup(dungeonlayout);
-outputARY<dungeonRowSize, dungeonColSize> (dungeonlayout);}
+outputARY<dungeonRowSize, dungeonColSize> (dungeonlayout);
+
+
+cout << endl<<endl<<dungeonlayout[1][0]<< endl<<endl;
+
+
+}
 
 
 void dungeonSetup(int dungeonlayout[dungeonRowSize][dungeonColSize])
 {
-    int value = 0;  // or start with 1 if you prefer
-    for(int row = 0; row < dungeonRowSize; row++){
-        for(int col = 0; col < dungeonColSize; col++){
-            dungeonlayout[row][col] = value;
-            value++;
-        }
-    }
+    fillARY<dungeonRowSize,dungeonColSize>(dungeonlayout);
+   
 }
 
 
